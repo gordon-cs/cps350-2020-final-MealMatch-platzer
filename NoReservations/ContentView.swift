ed4735f93ae9ce165f2aa0d9ea15b9075c13b211
Evan Platzer <evan.platzer@gordon.edu>
2020-04-12 13:31:51 -0500

    Initial Commit

---
+++ NoReservations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoReservations
+//
+//  Created by Evan Platzer on 4/12/20.
+//  Copyright © 2020 Evan Platzer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
